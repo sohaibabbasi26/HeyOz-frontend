44b2b6c478c68d43f02196acdf0b31038e370a36
Sohaib Abbasi <sobiabbasi22@gmail.com>
2026-02-25 15:38:33 +0500

    work: enhanced AEO and GEO: 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false, configFile: './tailwind.config.mjs' })]
+  site: "https://hey-oz-frontend.vercel.app/",
+  integrations: [tailwind({ applyBaseStyles: false, configFile: './tailwind.config.mjs' }), sitemap()],
+  output: 'static',
 });
